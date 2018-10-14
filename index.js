e57725a3e7f717f38ac6520ee294f1e1d58388d8
Moataz <moataz.mohammady@gmail.com>
2018-10-14 13:27:47 +0200

    Finalize example

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
-
+});
+var i = 0; 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
